4d8a8e2c4c011a8c15fdefe94b2621b7fb83eef3
Heiker Gil <gilheyker@gmail.com>
2019-08-12 21:10:53 -0500

    more js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
 
 
-let form = document.getElementById("trialform");git
-let fname = document.getElementById("fname");
-let lname = document.getElementById("lname");
-let email = document.getElementById("email");
-let password = document.getElementById("pass");
-function formSubmit() {
-    console.log(fname);
-    console.log(lname);
-    console.log(email);
-    console.log(password);
+function formSubmit(fsumit) {
+    console.log(fsumit);
+    
 }
