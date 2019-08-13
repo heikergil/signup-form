938f60c446827c4ee5df63c6a8627009bd7f3f24
Heiker Gil <gilheyker@gmail.com>
2019-08-12 20:30:49 -0500

    added some js funtionality

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+let form = document.getElementById("trialform");git
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let email = document.getElementById("email");
+let password = document.getElementById("pass");
+function formSubmit() {
+    console.log(fname);
+    console.log(lname);
+    console.log(email);
+    console.log(password);
+}
