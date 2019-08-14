717dead16e787653905e84a9e8247fcc30d665bd
Heiker Gil <gilheyker@gmail.com>
2019-08-13 21:31:57 -0500

    basicof js DOM manipulation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
+const form = document.getElementById("trialform");
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const fnameWarning = document.getElementById('fnameWarning');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
 
 
 function formSubmit(fsumit) {
-    console.log(fsumit);
+    console.log(fname.value);
+if (fname.value = " " || "" || null){
+    fnameWarning.classList.remove("hidden");
+    fname.classList.add('red_border');
+
+
+
+}
     
 }
