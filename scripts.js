2a3a37d4cbe91216188e78c986f0216378dd5fcc
Heiker Gil <gilheyker@gmail.com>
2019-08-14 17:02:46 -0500

    some js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 
 
+ $(document).ready(function() {
+
+document.getElementById("submit").addEventListener("click", function(event){
+event.preventDefault()
+});
+
+//  document.getElementById("trialform").addEventListener("click", function(event){
+//    event.preventDefault()
+//  });
+
 function formSubmit(fsumit) {
-    console.log(fsumit);
+
+    if (fname.value = "" | null) {
+        fname.classList.remove("hidden");
+    }
     
 }
+ });
