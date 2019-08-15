6c12fea384e371df261716cb5a9b4915a0b23764
Heiker Gil <gilheyker@gmail.com>
2019-08-14 21:19:54 -0500

    js functionality

--- scripts.js
+++ scripts.js
@@ -14,20 +14,24 @@ function formSubmit(fsumit) {
     
 if (fname.value = " " || "" || null){
     fnameWarning.classList.remove("hidden");
+    fnameWarning.innerHTML = "First name cannot be empty";
     fname.classList.add('red_border');
 };
 if (lname.value = " " || "" || null){
     lnameWarning.classList.remove("hidden");
+    lnameWarning.innerHTML = "Last name cannot be empty";
     lname.classList.add('red_border');
 };
 if (email.value = " " || "" || null){
     emailWarning.classList.remove("hidden");
+    emailWarning.innerHTML = "You must enter avalid email";
     email.classList.add('red_border');
     email.value = "";
     email.placeholder = "";
 };
 if (password.value = " " || "" || null){
     passwordWarning.classList.remove("hidden");
+    passwordWarning.innerHTML = "password cannot be empty";
     password.classList.add('red_border');
     password.value = "";
     password.placeholder = "";    
